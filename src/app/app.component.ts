76012c19f039f03334afbb5ea6b73e1045166c2b
André Antimonov <andre.antimonov@posteo.de>
2024-09-06 12:58:05 +0200

    Update(Angular): Add PrimeNG dependencies for PrimeNG components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { invoke } from '@tauri-apps/api/tauri'
+import { ButtonModule } from 'primeng/button'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [ButtonModule]
 })
 export class AppComponent {
   title = 'rust-audio-exploration';
-  greetingMessage = '';
-
-  async greet() {
-    this.greetingMessage = await invoke('greet', {name: 'Tauri'})
-  }
 }
