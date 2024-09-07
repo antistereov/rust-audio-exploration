4f66ee8ef0ddf4f2800610e4e7fff0113bfaecb6
antistereov <admin@stereov.io>
2024-09-08 01:59:46 +0200

    Feature(simple-button): Add simple-button component

---
+++ src/app/synthesizer/components/simple-button/simple-button.component.ts
@@ -0,0 +1,13 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+@Component({
+  selector: 'simple-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './simple-button.component.html',
+  styleUrl: './simple-button.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class SimpleButton {
+
+}
