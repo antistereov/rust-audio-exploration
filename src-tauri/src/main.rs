73f5827bbc5af152efc8a0f60662733a5ba0ca27
André Antimonov <andre.antimonov@posteo.de>
2024-09-07 22:29:56 +0200

    Update(tauri version): Update to tauri v2.0.0-rc

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,26 +3,44 @@
 mod ui;
 mod synth;
 mod audio_handler;
+use chrono::prelude::*;
 
+use tauri::Manager;
+use tauri_plugin_log::{Target, TargetKind};
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
 fn main() {
-
     tauri::Builder::default()
-        .plugin(tauri_plugin_log::Builder::default()
-            .target(tauri_plugin_log::Target::new(
-                tauri_plugin_log::TargetKind::Stdout,
-            ))
+        .plugin(
+            tauri_plugin_log::Builder::new()
+                .targets([
+                    Target::new(TargetKind::Stdout),
+                    Target::new(TargetKind::LogDir { file_name: None }),
+                    Target::new(TargetKind::Webview),
+            ])
+            .format(|out, message, record| {
+                out.finish(format_args!(
+                    "{} - {:<8} - {:<50} - {}",
+                    Local::now().format("%Y-%m-%d %H:%M:%S.%3f"),
+                    record.level(),
+                    record.target(),
+                    message
+                ))
+            })
             .build()
         )
-        .manage(std::sync::Mutex::new(audio_handler::audio_handler::AudioHandler::new()))
+        .setup(|app| {
+            let audio_handler = std::sync::Mutex::new(audio_handler::audio_handler::AudioHandler::new());
+            app.manage(audio_handler);
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![
             ui::audio_settings::audio_host::get_current_host,
             ui::audio_settings::audio_host::switch_host,
             ui::audio_settings::output_device::get_current_output_device_name,
             ui::audio_settings::output_device::select_output_device,
             ui::audio_settings::output_device::list_available_output_devices,
-            ])
-            .run(tauri::generate_context!())
-            .expect("error while running tauri application");
-
-    log::info!("Application started");
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- src-tauri/src/ui/audio_settings/output_device.rs
+++ src-tauri/src/ui/audio_settings/output_device.rs
@@ -2,16 +2,16 @@ use crate::audio_handler::{audio_handler::AudioHandler, audio_handler_error::Aud
 use std::sync::Mutex;
 
 #[tauri::command]
-pub fn get_current_output_device_name(audio_handler: tauri::State<'_, Mutex<AudioHandler>>) -> Option<String> {
+pub fn get_current_output_device_name(audio_handler: tauri::State<Mutex<AudioHandler>>) -> Option<String> {
     audio_handler.lock().unwrap().get_current_device_name()
 }
 
 #[tauri::command]
-pub fn list_available_output_devices(audio_handler: tauri::State<'_, Mutex<AudioHandler>>) -> Result<Vec<String>, AudioHandlerError> {
+pub fn list_available_output_devices(audio_handler: tauri::State<Mutex<AudioHandler>>) -> Result<Vec<String>, AudioHandlerError> {
     audio_handler.lock().unwrap().list_available_output_devices()
 }
 
 #[tauri::command]
-pub fn select_output_device(audio_handler: tauri::State<'_, Mutex<AudioHandler>>, device_name: String) -> Result<(), AudioHandlerError> {
+pub fn select_output_device(audio_handler: tauri::State<Mutex<AudioHandler>>, device_name: String) -> Result<(), AudioHandlerError> {
     audio_handler.lock().unwrap().select_output_device(&device_name)
 }
