6949a6ea07371be0622eb6c6d12d0ccfe926f625
antistereov <admin@stereov.io>
2024-09-08 02:00:38 +0200

    Feature(app): Add simple-button and simple-knob to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { AudioSettingsComponent } from './audio-settings/audio-settings.component';
+import { SimpleKnob } from './synthesizer/components/simple-knob/simple-knob.component';
+import { SimpleButton } from './synthesizer/components/simple-button/simple-button.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [ButtonModule, AudioSettingsComponent],
+  imports: [ButtonModule, AudioSettingsComponent, SimpleKnob, SimpleButton],
 })
 export class AppComponent {
   title = 'rust-audio-exploration';
